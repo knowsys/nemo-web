4adfeaac8bf1f146535d7e0a73fea9b3ea34242d
monsterkrampe <monsterkrampe@users.noreply.github.com>
2024-02-09 16:06:09 +0100

    Add very basic Evonne integration

--- src/components/rightPanel/executionPanel/ExecutionPanel.tsx
+++ src/components/rightPanel/executionPanel/ExecutionPanel.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import {
   Card,
   Button,
@@ -27,6 +27,7 @@ import "./ExecutionPanel.css";
 import { chooseFile } from "../../../chooseFile";
 import { downloadPredicate } from "./downloadPredicate";
 import { toastsSlice } from "../../../store/toasts";
+import { Evonne } from "./evonne/Evonne";
 
 function convertFileSize(size: number) {
   let index = size == 0 ? 0 : Math.floor(Math.log(size) / Math.log(1000));
@@ -64,6 +65,16 @@ export function ExecutionPanel() {
   const [tracingResult, setTracingResult] = useState<string | undefined>(
     undefined,
   );
+  
+  useEffect(
+    () => {
+      if (!!tracingResult) {
+        console.log(tracingResult);
+      }
+    },
+    [tracingResult],
+  );
+
   const [isTracingModalShown, setIsTracingModalShown] = useState(false);
 
   const programText = useAppSelector(selectProgramText);
@@ -460,11 +471,7 @@ export function ExecutionPanel() {
           {tracingResult === undefined ? (
             <>No results</>
           ) : (
-            <>
-              <code className="execution-panel-code-display">
-                {tracingResult}
-              </code>
-            </>
+            <Evonne data={tracingResult} />
           )}
         </Modal.Body>
         <Modal.Footer>
