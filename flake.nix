15fc205b680c042bb3e75d2245e2529546bf7cf0
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2025-01-29 16:09:49 +0100

    Add nix flake
    Flake lock file updates:
    
    • Added input 'dream2nix':
        'github:nix-community/dream2nix/8ce6284ff58208ed8961681276f82c2f8f978ef4' (2024-12-25)
    • Added input 'dream2nix/nixpkgs':
        follows 'nixpkgs'
    • Added input 'dream2nix/purescript-overlay':
        'github:thomashoneyman/purescript-overlay/4ad4c15d07bd899d7346b331f377606631eb0ee4' (2024-10-10)
    • Added input 'dream2nix/purescript-overlay/flake-compat':
        'github:edolstra/flake-compat/0f9255e01c2351cc7d116c072cb317785dd33b33' (2023-10-04)
    • Added input 'dream2nix/purescript-overlay/nixpkgs':
        follows 'dream2nix/nixpkgs'
    • Added input 'dream2nix/purescript-overlay/slimlock':
        'github:thomashoneyman/slimlock/cf72723f59e2340d24881fd7bf61cb113b4c407c' (2023-07-07)
    • Added input 'dream2nix/purescript-overlay/slimlock/nixpkgs':
        follows 'dream2nix/purescript-overlay/nixpkgs'
    • Added input 'dream2nix/pyproject-nix':
        'github:davhau/pyproject.nix/5a06a2697b228c04dd2f35659b4b659ca74f7aeb' (2023-12-13)
    • Added input 'nemo':
        'github:knowsys/nemo/b733a86977a53b1e5ef6eb8432577c2653aae893' (2025-01-28)
    • Added input 'nemo/nixpkgs':
        follows 'nixpkgs'
    • Added input 'nemo/rust-overlay':
        'github:oxalica/rust-overlay/27bb917a41480b6ceee8e42d32dfcc9ecc6fa6c6' (2025-01-27)
    • Added input 'nemo/rust-overlay/nixpkgs':
        follows 'nemo/nixpkgs'
    • Added input 'nemo/utils':
        follows 'utils'
    • Added input 'nemo-vscode-extension':
        'github:knowsys/nemo-vscode-extension/f9561dff01f0fa4ac5d063a048cfb10fcdd4639a' (2025-01-29)
    • Added input 'nemo-vscode-extension/dream2nix':
        follows 'dream2nix'
    • Added input 'nemo-vscode-extension/nemo':
        follows 'nemo'
    • Added input 'nemo-vscode-extension/nixpkgs':
        follows 'nixpkgs'
    • Added input 'nemo-vscode-extension/utils':
        follows 'utils'
    • Added input 'nixpkgs':
        'github:NixOS/nixpkgs/2b4230bf03deb33103947e2528cac2ed516c5c89' (2025-01-28)
    • Added input 'utils':
        'github:gytis-ivaskevicius/flake-utils-plus/6b10f51ff73a66bb29f3bc8151a59d217713f496' (2024-07-30)
    • Added input 'utils/flake-utils':
        'github:numtide/flake-utils/ff7b65b44d01cf9ba6a71320833626af21126384' (2023-09-12)
    • Added input 'utils/flake-utils/systems':
        'github:nix-systems/default/da67096a3b9bf56a91d16901293e51ba5b49a27e' (2023-04-09)

---
+++ flake.nix
@@ -0,0 +1,228 @@
+rec {
+  description = "Web frontend for the Nemo rule engine";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
+    dream2nix = {
+      url = "github:nix-community/dream2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nemo = {
+      url = "github:knowsys/nemo";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        utils.follows = "utils";
+      };
+    };
+
+    nemo-vscode-extension = {
+      url = "github:knowsys/nemo-vscode-extension";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        utils.follows = "utils";
+        dream2nix.follows = "dream2nix";
+        nemo.follows = "nemo";
+      };
+    };
+  };
+
+  outputs =
+    inputs@{
+      self,
+      utils,
+      nemo,
+      nemo-vscode-extension,
+      dream2nix,
+      ...
+    }:
+    utils.lib.mkFlake {
+      inherit self inputs;
+
+      channels.nixpkgs.overlaysBuilder = channels: [
+        nemo.overlays.default
+        nemo-vscode-extension.overlays.default
+      ];
+
+      overlays = {
+        default =
+          final: prev:
+          let
+            pkgs = self.packages.${final.system};
+          in
+          {
+            inherit (pkgs) nemo-web;
+          };
+        nemo = nemo.overlays.default;
+        nemo-vscode-extension = nemo-vscode-extension.overlays.default;
+      };
+
+      outputsBuilder =
+        channels:
+        let
+          pkgs = channels.nixpkgs;
+
+          npmMeta = builtins.fromJSON (builtins.readFile ./package.json);
+          inherit (npmMeta) version;
+
+          meta = {
+            inherit description;
+            homepage = npmMeta.repository.url;
+          };
+
+          nemo-web-source = pkgs.runCommandLocal "nemo-web-source" { } ''
+            mkdir -p $out/nemoVSIX
+            cp -R ${pkgs.lib.cleanSource ./.}/* $out
+            cp -R ${pkgs.nemo-wasm-bundler}/lib/node_modules/nemo-wasm/ $out/nemoWASMBundler
+            cp ${pkgs.nemo-vscode-extension-vsix}/nemo-*.vsix $out/nemoVSIX/nemo.vsix
+          '';
+
+          nemo-web = dream2nix.lib.evalModules {
+            packageSets.nixpkgs = pkgs;
+
+            modules = [
+              {
+                paths = {
+                  projectRoot = ./.;
+                  projectRootFile = "flake.nix";
+                  package = ./.;
+                };
+              }
+
+              (
+                {
+                  config,
+                  dream2nix,
+                  ...
+                }:
+                {
+                  name = "nemo-web";
+                  inherit version;
+
+                  imports = [
+                    dream2nix.modules.dream2nix.nodejs-package-lock-v3
+                    dream2nix.modules.dream2nix.nodejs-granular-v3
+                  ];
+
+                  mkDerivation = {
+                    src = nemo-web-source;
+
+                    installPhase = ''
+                      runHook preInstall
+
+                      cp -R dist $out
+
+                      runHook postInstall
+                    '';
+                  };
+
+                  deps =
+                    { nixpkgs, ... }:
+                    {
+                      inherit (nixpkgs) stdenv;
+                    };
+
+                  nodejs-package-lock-v3 = {
+                    packageLockFile = "${config.mkDerivation.src}/package-lock.json";
+                  };
+
+                  nodejs-granular-v3 = {
+                    buildScript = "NODE_OPTIONS=--max_old_space_size=4096 npm run build";
+                  };
+                }
+              )
+            ];
+          };
+        in
+        {
+          packages = {
+            inherit nemo-web;
+            default = nemo-web;
+          };
+
+          apps =
+            let
+              nemo-web-preview = utils.lib.mkApp {
+                drv = pkgs.writeShellApplication {
+                  name = "nemo-web-preview";
+
+                  runtimeInputs = [
+                    pkgs.nodejs
+                  ];
+
+                  text = ''
+                    TMPDIR="$(mktemp --directory)"
+                    trap 'rm -rf -- "$TMPDIR"' EXIT
+                    cd "$TMPDIR"
+                    cp -R ${self.packages.${pkgs.system}.nemo-web}/lib/node_modules/nemo-web/* .
+                    mkdir wrapper
+                    ln -s ../node_modules/vite/bin/vite.js wrapper/vite
+                    export PATH="''${PATH}:wrapper"
+                    chmod -R o+w .
+                    npm run preview
+                  '';
+                };
+              };
+            in
+            rec {
+              inherit nemo-web-preview;
+              default = nemo-web-preview;
+            };
+
+          devShells.default = dream2nix.lib.evalModules {
+            packageSets.nixpkgs = pkgs;
+
+            modules = [
+              {
+                paths = {
+                  projectRoot = ./.;
+                  projectRootFile = "flake.nix";
+                  package = ./.;
+                };
+              }
+
+              (
+                {
+                  config,
+                  dream2nix,
+                  ...
+                }:
+                {
+                  name = "nemo-web";
+                  inherit version;
+
+                  imports = [
+                    dream2nix.modules.dream2nix.nodejs-package-lock-v3
+                    dream2nix.modules.dream2nix.nodejs-devshell-v3
+                  ];
+
+                  mkDerivation = {
+                    src = nemo-web-source;
+
+                    nativeBuildInputs = [
+                      pkgs.nodejs
+                    ];
+
+                    buildPhase = "mkdir $out";
+                  };
+
+                  deps =
+                    { nixpkgs, ... }:
+                    {
+                      inherit (nixpkgs) stdenv;
+                    };
+
+                  nodejs-package-lock-v3 = {
+                    packageLockFile = "${config.mkDerivation.src}/package-lock.json";
+                  };
+                }
+              )
+            ];
+          };
+
+          formatter = channels.nixpkgs.nixfmt-rfc-style;
+        };
+
+    };
+}
