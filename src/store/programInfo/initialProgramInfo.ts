3aaf52c84b0db74dc5f302bb8d3abe4d0ff3f15e
monsterkrampe <monsterkrampe@users.noreply.github.com>
2025-10-22 10:23:25 +0200

    Fix first empty line in default editor text

--- src/store/programInfo/initialProgramInfo.ts
+++ src/store/programInfo/initialProgramInfo.ts
@@ -1,9 +1,8 @@
 import { ProgramInfo } from "./ProgramInfo";
 
 export const initialProgramInfo: ProgramInfo = {
-  programText: `
-% Write your Nemo program here, or look at the Examples above.
+  programText: `% Write your Nemo program here, or look at the Examples above.
 
 
-  `,
+`,
 };
